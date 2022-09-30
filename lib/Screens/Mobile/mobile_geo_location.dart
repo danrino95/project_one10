b3638b58e72a87b50d16beffee045fe5555e73bb
danrino <danrino95@gmail.com>
2022-09-30 11:07:46 +0530

    pan card, aadhar card , gst number verification

--- lib/Screens/Mobile/mobile_geo_location.dart
+++ lib/Screens/Mobile/mobile_geo_location.dart
@@ -1,5 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 
 class GeoLocationMobile extends StatefulWidget {
@@ -10,65 +11,64 @@ class GeoLocationMobile extends StatefulWidget {
 }
 
 class _GeoLocationMobileState extends State<GeoLocationMobile> {
-  String googleApikey = "";
-  double latitude = 27.666994; //latitude
-  double longitude = 85.309289; //longitude
-
-  String address = "";
-
-  @override
-  void initState() {
-    convertToAddress(latitude, longitude, googleApikey); //call convert to address
-    super.initState();
-  }
-
-  convertToAddress(double lat, double long, String apikey) async {
-    Dio dio = Dio();  //initilize dio package
-    String apiurl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$long&key=$apikey";
-
-    Response response = await dio.get(apiurl); //send get request to API URL
-
-    if(response.statusCode == 200){ //if connection is successful
-      Map data = response.data; //get response data
-      if(data["status"] == "OK"){ //if status is "OK" returned from REST API
-        if(data["results"].length > 0){ //if there is atleast one address
-          Map firstresult = data["results"][0]; //select the first address
-
-          address = firstresult["formatted_address"]; //get the address
-
-          //you can use the JSON data to get address in your own format
-
-          setState(() {
-            //refresh UI
-          });
-        }
-      }else{
-        print(data["error_message"]);
-      }
-    }else{
-      print("error while fetching geoconding data");
-    }
-  }
+  Position? _currentPosition;
+  String? _currentAddress;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: Text("Get Address from Google Map API"),
-          backgroundColor: Colors.redAccent,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Location"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_currentAddress != null) Text(
+                _currentAddress!
+            ),
+            if (_currentPosition != null) Text(
+                "LAT: ${_currentPosition?.latitude}, LNG: ${_currentPosition?.longitude}"
+            ),
+            ElevatedButton(
+              child: const Text("Get location"),
+              onPressed: () {
+                _getCurrentLocation();
+              },
+            ),
+          ],
         ),
-        body: Container(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              Text("Latitude: $latitude", style: TextStyle(fontSize: 25),),
-              Text("Longitude: $longitude", style: TextStyle(fontSize: 25)),
-              Text("Formatted Address: $address",
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,),
-            ],
-          ),
-        )
+      ),
     );
+  }
+
+  _getCurrentLocation() {
+    Geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best, forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _getAddressFromLatLng();
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition!.latitude,
+          _currentPosition!.longitude
+      );
+
+      Placemark place = placemarks[0];
+
+      setState(() {
+        _currentAddress = "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cloud_firestore
 import connectivity_plus_macos
 import firebase_auth
 import firebase_core
+import geolocator_apple
 import location
 import shared_preferences_macos
 
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
