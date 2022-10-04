3fbda3cc7470ccea11fdafa613b62c60f808a9ef
AnkurOjha <ojhankur@gmail.com>
2022-10-04 14:36:10 +0530

    PROPER WORKING GOV DOCS VALIDATOR

--- lib/Screens/Mobile/gov_auth.dart
+++ lib/Screens/Mobile/gov_auth.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:govt_documents_validator/govt_documents_validator.dart';
-import 'package:typroject/Screens/Mobile/mobile_geo_location.dart';
-
-import '../../utils/next_screen.dart';
 
 class gov_auth extends StatefulWidget {
   const gov_auth({Key? key}) : super(key: key);
@@ -89,28 +86,28 @@ class _gov_authState extends State<gov_auth> {
               SizedBox(
                 height: 30,
               ),
-          TextFormField(
-            maxLength: 15,
-            cursorColor: Colors.green,
-            decoration: InputDecoration(
-              labelText: 'PAN Number',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(25.0),
-                borderSide: BorderSide(),
+              TextFormField(
+                maxLength: 15,
+                cursorColor: Colors.green,
+                decoration: InputDecoration(
+                  labelText: 'PAN Number',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: BorderSide(),
+                  ),
+                ),
+                onFieldSubmitted: (value) {},
+                validator: (value) {
+                  if (panValidator.validate(value) == true) {
+                    return "Pan true";
+                  }
+                  return "Incorrect Pan Number";
+                },
               ),
-            ),
-            onFieldSubmitted: (value) {},
-            validator: (value) {
-              if (panValidator.validate(value) == true) {
-                return "Pan true";
-              }
-              return "Incorrect Pan Number";
-            },
-          ),
-          //box styling
-          SizedBox(
-            height:30,
-          ),
+              //box styling
+              SizedBox(
+                height:30,
+              ),
               //text input
               ElevatedButton(
 
@@ -121,19 +118,7 @@ class _gov_authState extends State<gov_auth> {
                   ),
                 ),
                 onPressed: () => _submit(),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  nextScreenReplace(
-                      context, const GeoLocationMobile());
-                },
-                child: Text("Email"),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
-              ),
+              )
             ],
           ),
         ),
